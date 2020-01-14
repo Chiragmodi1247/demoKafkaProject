2aef32c228ae8ca109d616bae7332d11c0211a38
Chirag Modi <mchirag1247@gmail.com>
2020-01-14 10:33:57 +0530

    changed repo name

--- src/main/java/com/training/demoKafkaProject/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/training/demoKafkaProject/service/impl/EmployeeServiceImpl.java
@@ -3,16 +3,13 @@ package com.training.demoKafkaProject.service.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.training.demoKafkaProject.entity.EmployeeEntity;
-import com.training.demoKafkaProject.repository.EmployeeRepository;
+import com.training.demoKafkaProject.repository.EmployeeRepositoryMongo;
 import com.training.demoKafkaProject.service.EmployeeService;
-import jdk.nashorn.internal.objects.annotations.Constructor;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PreDestroy;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -22,7 +19,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     private KafkaTemplate<String,String> kafkaTemplate;
 
     @Autowired
-    EmployeeRepository employeeRepository;
+    EmployeeRepositoryMongo employeeRepositoryMongo;
 
     @Override
     public void sendMessage(String TOPIC,EmployeeEntity employeeEntity) throws JsonProcessingException {
@@ -39,7 +36,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         EmployeeEntity employeeEntity = ow.readValue(message,EmployeeEntity.class);
 //        EmployeeEntity employeeEntity = new ObjectMapper().readValue(message,EmployeeEntity.class);
 //                System.out.println(employeeEntity);
-        employeeRepository.insert(employeeEntity);
+        employeeRepositoryMongo.insert(employeeEntity);
 
         System.out.println(employeeEntity.getFirstName());
         }
